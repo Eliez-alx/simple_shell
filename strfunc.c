2cfc04307fd40b90caa8d8fc0c434d8f38a1db99
Damian Nwodo <133491312+Damiangraphix@users.noreply.github.com>
2023-08-18 17:42:48 +0100

    Updated by Damian strfunc.c

--- strfunc.c
+++ strfunc.c
@@ -78,7 +78,7 @@ int _strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 /**
- *  *_strcat- concatinate two strings
+ *  *_strcat- concatenate two strings
  *  @dest: first string
  *  @src: second string
  *  Return: pointer
@@ -101,4 +101,67 @@ char  *_strcat(char *dest, char *src)
 	}
 	dest[r] = '\0';
 	return (dest);
+}
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
